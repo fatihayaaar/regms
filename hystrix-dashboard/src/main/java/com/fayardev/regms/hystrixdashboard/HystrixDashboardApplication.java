e4846d547be386fae4e715423dbf1c3b0edc746e
Fayar <fatihayaaar@gmail.com>
2023-12-13 22:16:54 +0300

    hystrix dashboard added

---
+++ hystrix-dashboard/src/main/java/com/fayardev/regms/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,15 @@
+package com.fayardev.regms.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrixDashboxard
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+
+}
