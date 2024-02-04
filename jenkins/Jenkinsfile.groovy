c18236e98d6cf6073ec108226b1d695b864d4e45
Fayar <fatihayaaar@gmail.com>
2024-02-04 19:42:38 +0300

    Jenkinsfile.groovy modified

--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -5,7 +5,9 @@
         stage('Checkout') {
             steps {
                 script {
-                    git branch: 'main', credentialsId: '2228b488-e0c8-41ab-8795-37afc7022a8b', url: 'https://github.com/fatihayaaar/regms.git'
+                    git branch: 'main',
+                            credentialsId: '2228b488-e0c8-41ab-8795-37afc7022a8b',
+                            url: 'https://github.com/fatihayaaar/regms.git'
                 }
             }
         }
