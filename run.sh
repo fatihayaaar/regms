251f611ca0ecac7e5703cfc756988ff661d41432
Fatih Ayar <fatihayaaar@gmail.com>
2024-09-07 13:27:50 +0300

    refactoring

--- run.sh
+++ run.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
-
-microservices=(
-    "config-server"
-    "eureka-server"
-    "gateway-server"
-    "user-service"
-    "profile-service"
-    "post-service"
-    "follow-service"
-    "like-service"
-    "comment-service"
-    "feed-service"
-    "admin-server"
-)
-
-for microservice in "${microservices[@]}"
-do
-  ./gradlew :"$microservice":build
-done
-
-docker-compose up -d &
 (
-  until docker exec cassandra cqlsh -e "SELECT table_name FROM system_schema.tables;" > /dev/null 2>&1; do
-    sleep 3
+  microservices=(
+      "config-server"
+      "eureka-server"
+      "gateway-server"
+      "user-service"
+      "profile-service"
+      "post-service"
+      "follow-service"
+      "like-service"
+      "comment-service"
+      "feed-service"
+      "admin-server"
+  )
+
+  for microservice in "${microservices[@]}"
+  do
+    ./gradlew :"$microservice":build
   done
-  docker exec cassandra cqlsh -f /docker-entrypoint-initdb.d/schema.cql
-) &
+
+  docker-compose up -d &
+  (
+    until docker exec cassandra cqlsh -e "SELECT table_name FROM system_schema.tables;" > /dev/null 2>&1; do
+      sleep 3
+    done
+    docker exec cassandra cqlsh -f /docker-entrypoint-initdb.d/schema.cql
+  )
+)
+docker-compose -f ./regms-client/docker-compose.yml up -d
